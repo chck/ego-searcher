70bc51ece861edb2d0d8a35c74f49e059ae71076
chck <iwazaki_yuki@cyberagent.co.jp>
2014-11-18 04:07:48 +0900

    add css file

--- app/ego-searcher.rb
+++ app/ego-searcher.rb
@@ -2,9 +2,16 @@
 #-*- coding:utf-8 -*-
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'services/followers'
 
 class EgoSearcher < Sinatra::Base
   get '/' do
+    @followers = (1..5)#Followers.all
     erb :index
+  end
+
+  post '/analysis' do
+    #@followers = Followers.all
+    redirect '/'
   end
 end
---
+++ app/services/followers.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#-*- coding:utf-8 -*-
+
+class Followers
+  def keyword
+
+  end
+
+  def all
+
+  end
+end
